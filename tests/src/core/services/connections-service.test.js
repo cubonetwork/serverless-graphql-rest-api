7d93da1ed06b36da55f1a4c0295f88258bb36197
Murilo Amêndola <muriloamendola@gmail.com>
2018-04-23 19:16:41 -0300

    Connections-service.js with 100 percent of coverage

--- tests/src/core/services/connections-service.test.js
+++ tests/src/core/services/connections-service.test.js
@@ -6,6 +6,7 @@ const ContactsRepository = require('../../../../src/core/repositories/dynamodb/c
 jest.mock('../../../../src/core/repositories/dynamodb/contacts-repository');
 
 describe('Connections Service', () => {
+  
   test('Returns an empty array when company does not have contacts', async () => {
     const companyId = 1;
     ContactsRepository.getByCompanyId.mockResolvedValue([]);
@@ -13,4 +14,73 @@ describe('Connections Service', () => {
 
     expect(connections).toEqual([]);
   });
+
+  test('Returns an array containing users, connections count and rating amount', async () => {
+    const companyId = 1;
+    ContactsRepository.getByCompanyId.mockResolvedValue([
+      {
+        connections: [
+          {
+            uid: '2',
+            rating: 1,
+            source: 'csv',
+            createdAt: 1523992555000
+          }
+        ]
+      },
+      {
+        connections: [
+          {
+            uid: '2',
+            rating: 1,
+            source: 'hubspot',
+            createdAt: 1523992555000
+          }
+        ]
+      },
+      {
+        connections: [
+          {
+            uid: '2',
+            rating: 3,
+            source: 'csv',
+            createdAt: 1523992555000
+          },
+          {
+            uid: '3',
+            rating: 5,
+            source: 'hubspot',
+            createdAt: 1523992555000
+          },
+          {
+            uid: '1',
+            rating: 2,
+            source: 'pipedrive',
+            createdAt: 1523992555000
+          }
+        ]
+      }
+    ]);
+
+    const connections = await ConnectionsService.getConnections(companyId, ContactsRepository);
+
+    expect(connections).toEqual([
+      {
+        uid: '2',
+        ratingSum: 5,
+        contactsAmount: 3
+      },
+      {
+        uid: '3',
+        ratingSum: 5,
+        contactsAmount: 1
+      },
+      {
+        uid: '1',
+        ratingSum: 2,
+        contactsAmount: 1
+      }
+    ]);
+  });
+
 });

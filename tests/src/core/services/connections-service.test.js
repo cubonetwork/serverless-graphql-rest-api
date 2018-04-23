bcc9043e81bbbbb4c78cf4e5dd5974db4790ccf1
Murilo Amêndola <muriloamendola@gmail.com>
2018-04-23 18:29:10 -0300

    Refactoring folder organization

--- tests/src/core/services/connections-service.test.js
+++ tests/src/core/services/connections-service.test.js
@@ -1,9 +1,9 @@
 import { resolve } from 'dns';
 
-const ConnectionsService = require('../../../core/services/connections-service');
-const ContactsRepository = require('../../../core/repositories/dynamodb/contacts-repository');
+const ConnectionsService = require('../../../../src/core/services/connections-service');
+const ContactsRepository = require('../../../../src/core/repositories/dynamodb/contacts-repository');
 
-jest.mock('../../../core/repositories/dynamodb/contacts-repository');
+jest.mock('../../../../src/core/repositories/dynamodb/contacts-repository');
 
 describe('Connections Service', () => {
   test('Returns an empty array when company does not have contacts', async () => {

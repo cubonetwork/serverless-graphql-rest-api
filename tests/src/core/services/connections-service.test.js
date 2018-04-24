4c84d0a3eb82d2c50c300e55a8b3954dc5035d4c
Murilo Amêndola <muriloamendola@gmail.com>
2018-04-24 16:22:14 -0300

    Refactoring soe tests

--- tests/src/core/services/connections-service.test.js
+++ tests/src/core/services/connections-service.test.js
@@ -3,66 +3,63 @@ const ContactsRepository = require('../../../../src/core/repositories/dynamodb/c
 
 jest.mock('../../../../src/core/repositories/dynamodb/contacts-repository');
 
+const FAKE_COMPANY_ID = 1;
+const FAKE_COMPANY_CONTACTS = [
+  {
+    connections: [
+      {
+        uid: '2',
+        rating: 1,
+        source: 'csv',
+        createdAt: 1523992555000
+      }
+    ]
+  },
+  {
+    connections: [
+      {
+        uid: '2',
+        rating: 1,
+        source: 'hubspot',
+        createdAt: 1523992555000
+      }
+    ]
+  },
+  {
+    connections: [
+      {
+        uid: '2',
+        rating: 3,
+        source: 'csv',
+        createdAt: 1523992555000
+      },
+      {
+        uid: '3',
+        rating: 5,
+        source: 'hubspot',
+        createdAt: 1523992555000
+      },
+      {
+        uid: '1',
+        rating: 2,
+        source: 'pipedrive',
+        createdAt: 1523992555000
+      }
+    ]
+  }
+];
+
 describe('Connections Service', () => {
-  
+
   test('Returns an empty array when company does not have contacts', async () => {
-    const companyId = 1;
     ContactsRepository.getByCompanyId.mockResolvedValue([]);
-    const connections = await ConnectionsService.getConnections(companyId);
+    const connections = await ConnectionsService.getConnections(FAKE_COMPANY_ID);
 
     expect(connections).toEqual([]);
   });
 
   test('Returns an array containing users, connections count and rating amount', async () => {
-    const companyId = 1;
-    ContactsRepository.getByCompanyId.mockResolvedValue([
-      {
-        connections: [
-          {
-            uid: '2',
-            rating: 1,
-            source: 'csv',
-            createdAt: 1523992555000
-          }
-        ]
-      },
-      {
-        connections: [
-          {
-            uid: '2',
-            rating: 1,
-            source: 'hubspot',
-            createdAt: 1523992555000
-          }
-        ]
-      },
-      {
-        connections: [
-          {
-            uid: '2',
-            rating: 3,
-            source: 'csv',
-            createdAt: 1523992555000
-          },
-          {
-            uid: '3',
-            rating: 5,
-            source: 'hubspot',
-            createdAt: 1523992555000
-          },
-          {
-            uid: '1',
-            rating: 2,
-            source: 'pipedrive',
-            createdAt: 1523992555000
-          }
-        ]
-      }
-    ]);
-
-    const connections = await ConnectionsService.getConnections(companyId);
-
-    expect(connections).toEqual([
+    const expected = [
       {
         uid: '2',
         ratingSum: 5,
@@ -78,52 +75,11 @@ describe('Connections Service', () => {
         ratingSum: 2,
         contactsAmount: 1
       }
-    ]);
-  });
+    ];
 
-  test('Should calculate rating amount properly', async () => {
-    const companyId = 1;
-    ContactsRepository.getByCompanyId.mockResolvedValue([
-      {
-        connections: [
-          {
-            uid: '2',
-            rating: 1,
-            source: 'csv',
-            createdAt: 1523992555000
-          }
-        ]
-      },
-      {
-        connections: [
-          {
-            uid: '2',
-            rating: 2,
-            source: 'hubspot',
-            createdAt: 1523992555000
-          }
-        ]
-      },
-      {
-        connections: [
-          {
-            uid: '2',
-            rating: 4,
-            source: 'gmail',
-            createdAt: 1523992555000
-          }
-        ]
-      }
-    ]);
-
-    const connections = await ConnectionsService.getConnections(companyId);
-
-    expect(connections[0]).toEqual({
-      uid: '2',
-      ratingSum: 7,
-      contactsAmount: 3
-    })
-
+    ContactsRepository.getByCompanyId.mockResolvedValue(FAKE_COMPANY_CONTACTS);
+    const connections = await ConnectionsService.getConnections(FAKE_COMPANY_ID);
+    expect(connections).toEqual(expected);
   });
 
 });
--- tests/src/graphql/schema.test.js
+++ tests/src/graphql/schema.test.js
@@ -4,7 +4,7 @@ const schema = require('../../../src/graphql/schema');
 
 const casesToTest = [
   {
-    description: 'getCompanyInfo',
+    description: 'Should return campany name',
     query: `
       query {
         getCompanyInfo(id: "2") {
@@ -13,45 +13,72 @@ const casesToTest = [
       }
     `,
     expected: { data: { getCompanyInfo: { name: 'Itaú' } } }
+  },
+  {
+    description: 'Should return campany name and contacts',
+    query: `
+      query {
+        getCompanyInfo(id: "2") {
+          name,
+          contacts {
+            name
+          }
+        }  
+      }
+    `,
+    expected: { 
+      data: { 
+        getCompanyInfo: { 
+          name: 'Itaú',
+          contacts: [
+            {
+              name: 'Jon'
+            }
+          ] 
+        } 
+      } 
+    }
   }
 ];
 
 describe('Schema', () => {
-  
-  addMockFunctionsToSchema({
-    schema,
-    mocks: {
-      Company: () => ({
-        id: '1',
-        name: 'Itaú',
-        contacts: () => new MockList([
-          {
-            email: 'jon@cubo.network',
-            name: 'Jon',
-            companyId: '1',            
-            connections: [
-              {
-                uid: '2',
-                rating: 3,
-                source: 'csv'
-              },
-              {
-                uid: '3',
-                rating: 5,
-                source: 'hubspot'
-              },
-              {
-                uid: '1',
-                rating: 2,
-                source: 'pipedrive'
-              }
-            ]
-          }
-        ])
-      })
-    }
+ 
+  beforeAll(() => {
+    addMockFunctionsToSchema({
+      schema,
+      mocks: {
+        Company: () => ({
+          id: '1',
+          name: 'Itaú',
+          contacts: [
+            {
+              email: 'jon@cubo.network',
+              name: 'Jon',
+              companyId: '1',            
+              connections: [
+                {
+                  uid: '2',
+                  rating: 3,
+                  source: 'csv'
+                },
+                {
+                  uid: '3',
+                  rating: 5,
+                  source: 'hubspot'
+                },
+                {
+                  uid: '1',
+                  rating: 2,
+                  source: 'pipedrive'
+                }
+              ]
+            }
+          ]
+        })
+      }
+    });
   });
-
+  
   test('Has valid type definitions', async () => {
     expect(async () => {
       const MockServer = mockServer(schema);
